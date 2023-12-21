7fc237d1241fc53c33fcf6cc6bee6adb272726f6
ADARSH RAJ <90273605+adarsh02raj@users.noreply.github.com>
2023-12-21 23:27:25 +0530

    Update common.jsx

--- src/common/common.jsx
+++ src/common/common.jsx
@@ -1,5 +1,5 @@
 export const API_URL =
-  "https://corsproxy.org/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+  "https://corsproxy.org/?https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Frestaurants%2Flist%2Fv5%3Flat%3D12.9351929%26lng%3D77.62448069999999%26is-seo-homepage-enabled%3Dtrue%26page_type%3DDESKTOP_WEB_LISTING";
 
 export const imgURLBanner =
   "https://corsproxy.org/?https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
